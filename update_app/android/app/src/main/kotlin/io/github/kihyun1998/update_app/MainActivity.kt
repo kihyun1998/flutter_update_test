d6ce33fed22d881b05628b9028a118cf8f45190e
박기현 <kihyun1998@naver.com>
2024-03-13 08:39:01 +0900

    Initial commit

---
+++ update_app/android/app/src/main/kotlin/io/github/kihyun1998/update_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.github.kihyun1998.update_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ update_app/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <auto_updater/auto_updater_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  AutoUpdaterPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("AutoUpdaterPlugin"));
+}
