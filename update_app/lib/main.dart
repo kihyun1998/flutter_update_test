966e6bc045dba50b65611e5c457a1268d370167b
박기현 <kihyun1998@naver.com>
2024-03-13 15:15:12 +0900

    server update

--- update_app/lib/main.dart
+++ update_app/lib/main.dart
@@ -3,12 +3,6 @@ import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  String feedURL = 'http://127.0.0.1:8000/static/appcast.xml';
-
-  // 로그 남겨야할 부분이다.
-  await autoUpdater.setFeedURL(feedURL);
-  await autoUpdater.checkForUpdates();
-  await autoUpdater.setScheduledCheckInterval(3600);
 
   runApp(const MyApp());
 }
@@ -26,8 +20,22 @@ class MyApp extends StatelessWidget {
         ),
         home: Scaffold(
           appBar: AppBar(title: const Text("App Update Test")),
-          body: const Center(
-            child: Text("Before Update"),
+          body: Center(
+            child: Column(
+              children: [
+                const Text("After Update"),
+                ElevatedButton(
+                    onPressed: () async {
+                      String feedURL = 'http://127.0.0.1:8000/dist/appcast.xml';
+
+                      // 로그 남겨야할 부분이다.
+                      await autoUpdater.setFeedURL(feedURL);
+                      await autoUpdater.checkForUpdates();
+                      await autoUpdater.setScheduledCheckInterval(3600);
+                    },
+                    child: const Text("Is update?")),
+              ],
+            ),
           ),
         ));
   }
